96c475b4b86e7cf5c0c24a6a25eee4f661bf6728
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-03 21:29:06 +0400

    392. Is Subsequence

---
+++ problems/392.IsSubsequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution392
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.Length && j < t.Length)
+        {
+            if (s[i] == t[j])
+                i++;
+            j++;
+        }
+
+        return i == s.Length;
+    }
+}
+
