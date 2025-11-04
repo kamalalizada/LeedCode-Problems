fc907939a8c0bae39718288f84f955a43e2e79aa
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-04 23:51:38 +0400

    206. Reverse Linked List

---
+++ problems/206. ReverseLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution206
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null)
+        {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+}
+
