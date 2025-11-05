7df3a109da3694d81ec8fff93ee956e052a9a684
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-05 23:58:56 +0400

    405. Convert a Number to Hexadecimal

---
+++ problems/405. ConvertaNumbertoHexadecimal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution405
+{
+    public string ToHex(int num)
+    {
+        if (num == 0) return "0";
+
+        uint n = (uint)num;
+        string hexChars = "0123456789abcdef";
+        StringBuilder sb = new StringBuilder();
+
+        while (n != 0)
+        {
+            int digit = (int)(n & 0xF);
+            sb.Insert(0, hexChars[digit]);
+            n >>= 4;
+        }
+
+        return sb.ToString();
+    }
+}
