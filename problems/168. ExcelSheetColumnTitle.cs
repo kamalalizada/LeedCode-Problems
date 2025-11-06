ce67a15006ecceac2669e00be4f49dea34f3832f
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-06 23:50:19 +0400

    168. Excel Sheet Column Title

---
+++ problems/168. ExcelSheetColumnTitle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution168
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        string result = "";
+
+        while (columnNumber > 0)
+        {
+            columnNumber--;
+            int remainder = columnNumber % 26;
+            result = (char)(remainder + 'A') + result;
+            columnNumber /= 26;
+        }
+
+        return result;
+    }
+}
+
