99f4acfd02bbfd36ce8745eadf1457bd5a13a674
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-07 22:59:47 +0400

    191. Number of 1 Bits

---
+++ problems/191.Numberof1Bits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution191
+{
+    public int HammingWeight(int n)
+    {
+        List<int> list = new List<int>();
+        while(n >= 1)
+        {
+            if(n%2 == 0)
+            {
+                list.Add(0);
+                n = n / 2;
+            }
+            else
+            {
+                list.Add(1);
+                n=(n-1)/2;
+            }
+        } 
+
+        return list.Count(x=>x==1);
+
+    }
+}
