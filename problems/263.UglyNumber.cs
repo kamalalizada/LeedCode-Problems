bc2c3ea27f3c7737261ac56c86e6d77bcb38083d
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-11 23:33:43 +0400

    263. Ugly Number

---
+++ problems/263.UglyNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution263
+{
+    public bool IsUgly(int n)
+    {
+        if (n <= 0) return false;
+
+        int[] primes = { 2, 3, 5 };
+        foreach (int p in primes)
+        {
+            while (n % p == 0)
+            {
+                n /= p;
+            }
+        }
+
+        return n == 1;
+    }
+}
+
