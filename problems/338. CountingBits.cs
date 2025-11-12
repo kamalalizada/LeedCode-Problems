55f2ecd265c7e551584ac44915d39cc3b7d9017a
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-12 23:10:03 +0400

    338. Counting Bits

---
+++ problems/338. CountingBits.cs
@@ -0,0 +1,34 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution338
+{
+    public int[] CountBits(int n)
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            int num = i;
+            int a = 0;
+            while (num > 0)
+            {
+                if (num % 2 == 1)
+                {
+                    a += 1;
+                    num = num / 2;
+
+
+                }
+                else
+                {
+                    a = a;
+                    num = num / 2;
+                }
+
+            }
+            list.Add(a);
+        }
+
+        return list.ToArray();
+
+    }
+}
