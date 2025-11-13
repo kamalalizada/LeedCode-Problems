5a9fb3016057e42d2afe44415a80d1385a9735b7
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-13 23:40:33 +0400

    367. Valid Perfect Square

---
+++ problems/367. ValidPerfectSquare.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution367
+{
+    public bool IsPerfectSquare(int num)
+    {
+
+        int a =(int)Math.Sqrt(num);
+        
+        if(a*a == num)
+            return true;
+        else
+            return false;
+
+
+    }
+}
