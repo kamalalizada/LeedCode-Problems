95d819223d9e09e39320ca75d202abf4b0599683
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-15 23:48:59 +0400

    476. Number Complement

---
+++ problems/476. NumberComplement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution476
+{
+    public int FindComplement(int num)
+    {
+        List<int> result = new List<int>();
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+                result.Add(1);
+            else
+                result.Add(0);
+
+            num /= 2;
+        }
+
+        int resultNum = 0;
+        for (int i = 0; i < result.Count; i++)
+        {
+            resultNum += result[i] << i;
+        }
+
+        return resultNum;
+    }
+}
+
