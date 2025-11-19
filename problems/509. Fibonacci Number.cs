e4cf9281de2e3b64daa74aa3de081177c6ab55d1
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-19 23:35:53 +0400

    509. Fibonacci Number

---
+++ problems/509. Fibonacci Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution509
+{
+    public int Fib(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
+
