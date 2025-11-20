3b71b5208a3cc2c94eb230715e3ab2f81b36b5fe
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-20 23:19:33 +0400

    704. Binary Search

---
+++ problems/704. Binary Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution704
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2; 
+
+            if (nums[mid] == target)
+            {
+                return mid; 
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1; 
+            }
+            else
+            {
+                right = mid - 1; 
+            }
+        }
+
+        return -1;
+    }
+}
+
