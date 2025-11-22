7242abe2472da5594d30a4c943531fe38615d8c3
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-22 23:59:19 +0400

    868. Binary Gap

---
+++ problems/868. Binary Gap.cs
@@ -0,0 +1,39 @@
+﻿namespace LeedCode_Problems.problems;
+using System.Collections.Generic;
+
+
+public class Solution868
+{
+    public int BinaryGap(int n)
+    {
+        List<int> bits = new List<int>();
+
+        while (n > 0)
+        {
+            bits.Add(n % 2);
+            n = n / 2;
+        }
+
+        int last = -1;
+        int maxGap = 0;
+
+        for (int i = 0; i < bits.Count; i++)
+        {
+            if (bits[i] == 1)
+            {
+                if (last != -1)
+                {
+                    int gap = i - last;
+                    if (gap > maxGap)
+                        maxGap = gap;
+                }
+
+                last = i;
+            }
+        }
+
+        return maxGap;
+    }
+}
+
+
