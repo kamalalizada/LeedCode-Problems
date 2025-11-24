26540d82d40e93dfe07bf7de7e302026459f4d95
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-24 23:43:11 +0400

    709. To Lower Case

---
+++ problems/709. To Lower Case.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution709
+{
+    public string ToLowerCase(string s)
+    {
+
+        return s.ToLower();
+
+    }
+}
