46dc1fdccd669b1e4da18b330654e2cc51a5cf0a
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-25 23:28:24 +0400

    520. Detect Capital

---
+++ problems/520.DetectCapital.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution520
+{
+    public bool DetectCapitalUse(string word)
+    {
+        if (word == word.ToUpper()) return true;
+
+        if (word == word.ToLower()) return true;
+
+        if (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower())
+            return true;
+
+        return false;
+    }
+}
+
