e977a7fd44eea3310e7d8acf5ee72e26f69df56d
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-26 23:39:22 +0400

    415. Add Strings

---
+++ problems/415. AddStrings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution415
+{
+    public string AddStrings(string num1, string num2)
+    {
+        int i = num1.Length - 1;
+        int j = num2.Length - 1;
+        int carry = 0;
+        var result = new System.Text.StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry != 0)
+        {
+            int n1 = i >= 0 ? num1[i] - '0' : 0;
+            int n2 = j >= 0 ? num2[j] - '0' : 0;
+            int sum = n1 + n2 + carry;
+            result.Insert(0, sum % 10); 
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+
+        return result.ToString();
+    }
+}
+
