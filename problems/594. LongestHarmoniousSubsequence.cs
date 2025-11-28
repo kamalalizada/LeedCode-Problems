5738575feee1e5ebaa5b3870b2e70a493a9afb61
Kamal Alizada <alizadakamal07@gmail.com>
2025-11-28 23:02:04 +0400

    594. Longest Harmonious Subsequence

---
+++ problems/594. LongestHarmoniousSubsequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution594
+{
+    public int FindLHS(int[] nums)
+    {
+        Dictionary<int, int> count = new Dictionary<int, int>();
+
+        foreach (int num in nums)
+        {
+            if (count.ContainsKey(num))
+            {
+                count[num]++;
+            }
+            else
+            {
+                count[num] = 1;
+            }
+        }
+
+        int maxLength = 0;
+
+        foreach (int key in count.Keys)
+        {
+            if (count.ContainsKey(key + 1))
+            {
+                maxLength = Math.Max(maxLength, count[key] + count[key + 1]);
+            }
+        }
+
+        return maxLength;
+    }
+}
