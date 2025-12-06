1daf2a2b86594f534d5309e625e654b0e58157f7
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-06 23:53:14 +0400

    575. Distribute Candies

---
+++ problems/575. DistributeCandies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution575
+{
+    public int DistributeCandies(int[] candyType)
+    {
+        HashSet<int> set = new HashSet<int>(candyType);
+
+        int differentTypes = set.Count;
+        int maxEat = candyType.Length / 2;
+
+        return Math.Min(differentTypes, maxEat);
+    }
+}
+
