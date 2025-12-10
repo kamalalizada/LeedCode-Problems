001ada661090d029e1556211edd326a3e54ff79c
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-10 23:51:50 +0400

    680. Valid Palindrome II

---
+++ problems/680.ValidPalindromeII.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LeedCode_Problems.problems;
+public class Solution680
+{
+    public bool ValidPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                return IsPalindrome(s, left + 1, right) ||
+                       IsPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private bool IsPalindrome(string s, int left, int right)
+    {
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
