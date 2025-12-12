ce8c89571f15aa98ffc9aca40543536546deec72
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-12 22:07:36 +0400

    717. 1-bit and 2-bit Characters

---
+++ problems/717.1-bitand2-bitCharacters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution717
+{
+    public bool IsOneBitCharacter(int[] bits)
+    {
+        int result = 0;
+
+        for (int i = 0; i < bits.Length; i++)
+        {
+            if (bits[i] == 0)
+            {
+                result = 1;
+                
+            }
+            else if (bits[i] == 1)
+            {
+                result = 2;
+                i = i + 1;
+            }
+        }
+
+        if (result == 1) 
+            return true;
+        return false;
+    }
+}
