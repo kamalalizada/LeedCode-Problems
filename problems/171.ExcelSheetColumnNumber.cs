004b7c97f588618c8ba6607f8196c3ad97a65601
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-13 17:16:55 +0400

    171. Excel Sheet Column Number

---
+++ problems/171.ExcelSheetColumnNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution171
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int result = 0;
+
+        foreach (char c in columnTitle)
+        {
+            result = result * 26 + (c - 'A' + 1);
+        }
+
+        return result;
+    }
+}
