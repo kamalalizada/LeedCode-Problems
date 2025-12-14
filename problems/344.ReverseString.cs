d9ff7ec48e5e2313a408391adfbd3304b12e6dbf
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-14 22:45:18 +0400

    344. Reverse String

---
+++ problems/344.ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution344
+{
+    public void ReverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
+
