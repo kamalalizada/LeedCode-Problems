af93f22a3a4268c7c0bf0a0baf3cdd9260ec0804
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-15 23:41:47 +0400

    202. Happy Number

---
+++ problems/202.HappyNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution202
+{
+    public bool IsHappy(int n)
+    {
+        HashSet<int> seen = new HashSet<int>();
+
+        while (n != 1 && !seen.Contains(n))
+        {
+            seen.Add(n);
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int a = n % 10;
+                sum += a * a;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+}
+
