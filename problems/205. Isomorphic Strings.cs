f3735600c2369c66fffd6fb0f5f6af2fcb8bae1b
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-16 23:20:31 +0400

    public class Solution {     public bool IsIsomorphic(string s, string t) {         if (s.Length != t.Length)             return false;
            Dictionary<char, char> mapST = new Dictionary<char, char>();
            Dictionary<char, char> mapTS = new Dictionary<char, char>();
    
            for (int i = 0; i < s.Length; i++) {
                char cs = s[i];
                char ct = t[i];
    
                if (mapST.ContainsKey(cs)) {
                    if (mapST[cs] != ct)
                        return false;
                } else {
                    mapST[cs] = ct;
                }
    
                if (mapTS.ContainsKey(ct)) {
                    if (mapTS[ct] != cs)
                        return false;
                } else {
                    mapTS[ct] = cs;
                }
            }
    
            return true;
        }
    }

---
+++ problems/205. Isomorphic Strings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution205
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        Dictionary<char, char> mapST = new Dictionary<char, char>();
+        Dictionary<char, char> mapTS = new Dictionary<char, char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char cs = s[i];
+            char ct = t[i];
+
+            if (mapST.ContainsKey(cs))
+            {
+                if (mapST[cs] != ct)
+                    return false;
+            }
+            else
+            {
+                mapST[cs] = ct;
+            }
+
+            if (mapTS.ContainsKey(ct))
+            {
+                if (mapTS[ct] != cs)
+                    return false;
+            }
+            else
+            {
+                mapTS[ct] = cs;
+            }
+        }
+
+        return true;
+    }
+}
+
