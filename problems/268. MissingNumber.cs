f3839ab7a6cfbbb3ffd7ff5d8a7589fa4c045d95
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-17 22:47:26 +0400

    268. Missing Number

---
+++ problems/268. MissingNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution268
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            actualSum += nums[i];
+        }
+
+        return expectedSum - actualSum;
+    }
+}
+
