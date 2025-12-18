a317224595ff02689b92a0c86f64b0a20a0aee6d
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-18 23:36:56 +0400

    349. Intersection of Two Arrays

---
+++ problems/349. Intersection of Two Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution349
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set1 = new HashSet<int>(nums1);
+        HashSet<int> intersection = new HashSet<int>();
+
+        foreach (int num in nums2)
+        {
+            if (set1.Contains(num))
+            {
+                intersection.Add(num);
+            }
+        }
+
+        return new List<int>(intersection).ToArray();
+    }
+}
