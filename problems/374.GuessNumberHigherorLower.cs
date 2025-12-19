a970a5a79dd4cd29b97d7acf7dd59a57cb5eeeac
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-19 23:36:04 +0400

    374. Guess Number Higher or Lower

---
+++ problems/374.GuessNumberHigherorLower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution374 : GuessGame
+{
+    public int GuessNumber(int n)
+    {
+        int low = 1;
+        int high = n;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+
+            if (res == 0)
+                return mid;
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return -1;
+    }
+}
