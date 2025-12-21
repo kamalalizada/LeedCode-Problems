430fed50f0aca277b850e73f80107635a52a1de2
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-21 23:03:03 +0400

    441. Arranging Coins

---
+++ problems/441. ArrangingCoins.cs
@@ -0,0 +1,32 @@
+﻿namespace LeedCode_Problems.problems;
+public class Solution441
+{
+
+    public int ArrangeCoins(int n)
+    {
+
+        long sum = 1;
+        int result = 1;
+        while (true)
+        {
+            if (sum < n)
+            {
+                sum = sum + result + 1;
+                result = result + 1;
+            }
+
+            else if (sum == n)
+                return result;
+
+            else
+            {
+                return result - 1;
+            }
+
+        }
+
+    }
+}
+
+
+
