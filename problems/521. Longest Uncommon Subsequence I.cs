32f80c158c91b26f21072665dea7885abb89fc90
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-24 23:47:25 +0400

    521. Longest Uncommon Subsequence I

---
+++ problems/521. Longest Uncommon Subsequence I.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution521
+{
+    public int FindLUSlength(string a, string b)
+    {
+        if (a == b) return -1;
+        return Math.Max(a.Length, b.Length);
+    }
+}
+
