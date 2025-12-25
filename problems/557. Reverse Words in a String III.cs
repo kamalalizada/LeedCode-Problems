0bee64f8d2afe0a0d1c6692bec162891eeac6a09
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-25 23:35:42 +0400

    557. Reverse Words in a String III

---
+++ problems/557. Reverse Words in a String III.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution557
+{
+    public string ReverseWords(string s)
+    {
+        var words = s.Split(' ');
+        var result = new StringBuilder();
+
+        foreach (var word in words)
+        {
+            var reversedWord = new string(word.Reverse().ToArray());
+            result.Append(reversedWord).Append(' ');
+        }
+
+        return result.ToString().TrimEnd();
+    }
+}
+
