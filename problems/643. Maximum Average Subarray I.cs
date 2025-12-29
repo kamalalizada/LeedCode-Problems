d0ae41fc29e77fcd499920308b4e67b0d1077b89
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-29 23:00:47 +0400

    643. Maximum Average Subarray I

---
+++ problems/643. Maximum Average Subarray I.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution643
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        long windowSum = 0;
+
+
+        for (int i = 0; i < k; i++)
+            windowSum += nums[i];
+
+        long maxSum = windowSum;
+
+        for (int i = k; i < nums.Length; i++)
+        {
+            windowSum += nums[i] - nums[i - k];
+            if (windowSum > maxSum) maxSum = windowSum;
+        }
+
+        return (double)maxSum / k;
+    }
+}
+
