41600dfa331e0335509867c05fb4ec1afdadbce6
Kamal Alizada <alizadakamal07@gmail.com>
2025-12-30 23:32:03 +0400

    728. Self Dividing Numbers

---
+++ problems/728. Self Dividing Numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeedCode_Problems.problems;
+public class Solution728
+{
+    public IList<int> SelfDividingNumbers(int left, int right)
+    {
+        List<int> result = new List<int>();
+
+        for (int num = left; num <= right; num++)
+        {
+            int temp = num;
+            bool isSelfDividing = true;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+
+                if (digit == 0 || num % digit != 0)
+                {
+                    isSelfDividing = false;
+                    break;
+                }
+
+                temp /= 10;
+            }
+
+            if (isSelfDividing)
+                result.Add(num);
+        }
+
+        return result;
+    }
+}
